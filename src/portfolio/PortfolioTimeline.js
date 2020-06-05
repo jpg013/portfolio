76cf7c245298a7ece2991977ae8f63db4c5fbc17
Justin Graber <jpg013@gmail.com>
2020-06-05 14:01:24 -0500

    cleanup, added items to portfolio

--- src/portfolio/PortfolioTimeline.js
+++ src/portfolio/PortfolioTimeline.js
@@ -1,6 +1,53 @@
 import React from 'react';
 import cx from 'classnames';
 
+const renderOverview = (overview=[]) => {
+  if (!overview.length) {
+    return null;
+  }
+
+  return (
+    <div className="project-overview">
+      <ul>
+        {
+          overview.map((curr, idx) => {
+            return (
+              <li key={ idx }>{ curr }</li>
+            );
+          })
+        }
+      </ul>
+    </div>
+  );
+};
+
+const renderTechnologies = (tech=[]) => {
+  if (!tech.length) {
+    return null;
+  }
+
+  return (
+    <div className="project-technologies">
+      <h3>technologies</h3>
+      <ul>
+        {
+          tech.map((curr, idx) => {
+            return (
+              <li key={ idx }>
+                <a target="_blank noopener noreferrer" href={ tech.url }>
+                  <basic-chip>
+                    { curr.name }
+                  </basic-chip>  
+                </a>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  );
+}
+
 const PortfolioTimeline = ({ data, showBorder }) => {
   const cns = cx('project-contents', {
     'no-border': !showBorder,
@@ -14,41 +61,14 @@ const PortfolioTimeline = ({ data, showBorder }) => {
       <div className={ cns }>
         <div className="project-details">
           <div className="project-title">
-            <a target="_blank" href="<%= project.url %>">{ data.name } </a>
+            <a target="_blank noopener noreferrer" href={ data.url }>{ data.name } </a>
           </div>
           <div 
             className="project-text" 
             dangerouslySetInnerHTML={{ __html: data.description_html }}>
           </div>
-          <div className="project-overview">
-            <ul>
-              {
-                data.overview.map((curr, idx) => {
-                  return (
-                    <li key={ idx }>{ curr }</li>
-                  );
-                })
-              }
-            </ul>
-          </div>
-          <div className="project-technologies">
-            <h3>technologies</h3>
-            <ul>
-              {
-                data.tech.map((curr, idx) => {
-                  return (
-                    <li key={ idx }>
-                      <a target="_blank" href="<%= tech.url %>">
-                        <basic-chip>
-                          { curr.name }
-                        </basic-chip>  
-                      </a>
-                    </li>
-                  )
-                })
-              }
-            </ul>
-          </div>
+          { renderOverview(data.overview) }
+          { renderTechnologies(data.tech) }
         </div>
         <div className="project-gallery">
           {
